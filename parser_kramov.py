fbaf06979524abf9075138fc16a1793a2363bb51
Memris <mr.englishcat@gmail.com>
2024-05-02 06:08:26 +0300

    get data for item

--- parser_kramov.py
+++ parser_kramov.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import json
 import re
 import os
@@ -6,6 +7,18 @@ import time
 import requests
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+
+
+@dataclass
+class KramovDataClass:
+    brand_name: str = None
+    brand_url: str = None
+    # brand_img: str = None
+    brand_description: str = None
+    preview: str = None
+
+    # def __str__(self):
+    #     return f"{self.brand_name}\n\t{self.brand_url}\n\t\t{self.brand_img}\n\t\t\t{self.brand_description}"
 
 
 class KramovParser:
@@ -33,15 +46,16 @@ class KramovParser:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
     @staticmethod
-    def read_from_json(path):
+    def read_from_json(path, issearch=True):
         if not __class__.check_file(path):
             print(f'[ALERT] Файл по пути: {path} не найден!')
             return
         with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
-            for item in data:
-                if 'urls' in item:
-                    item['urls'].clear()
+            if issearch:
+                for item in data:
+                    if 'urls' in item:
+                        item['urls'].clear()
             return data
 
     def get_categories(self):
@@ -68,10 +82,30 @@ class KramovParser:
 
         return result
 
+    def get_all_data_from_url(self, url):
+        response = requests.get(url, headers=self.HEADERS)
+        soup = BeautifulSoup(response.content, 'lxml')
+        position = KramovDataClass()
+        position.brand_name = soup.find('a', class_='brand__picture').get('title')
+        # position.brand_img = soup.find('a', class_='brand__picture').find('img').get('')
+        position.brand_img_url = soup.find('a', class_='brand__picture').find('img').get('src')
+        position.preview = [''.join(filter(lambda char: char not in ('\n', '\t', '\r'), item.text.strip())) for item in soup.find('div', class_='preview-text').find_all('tr')]
+
+        print(position)
+
+    def get_data(self, json_data):
+        print(json_data)
+        for row in json_data:
+            for url in row['urls']:
+                self.get_all_data_from_url(self.BASE_URL + url)
+
+                break
+            break
+
     def get_next_page(self, soup):
         return soup.find('div', class_='nums').find('a', class_='flex-next').get('href')
 
-    def get_items_from_page(self, soup, index, categories: json):
+    def get_all_items_from_pages(self, soup, index, categories: json):
         elements = soup.find_all('div',
                                  class_='col-lg-3 col-md-4 col-sm-6 col-xs-6 col-xxs-12 item item-parent item_block')
         categories[index].setdefault('urls', [])
@@ -90,7 +124,7 @@ class KramovParser:
                 response = requests.get(url, headers=self.HEADERS)
                 if response.status_code == 200:
                     soup = BeautifulSoup(response.content, 'lxml')
-                    self.get_items_from_page(soup, index, categories)
+                    self.get_all_items_from_pages(soup, index, categories)
                     try:
                         pagination = self.get_next_page(soup)
                         url = self.BASE_URL + pagination
@@ -106,9 +140,11 @@ class KramovParser:
     def run(self):
         # categories = self.get_categories()
         # self.write_to_json(f'{self.BASE_FILE_PATH}/json/kramov_categories.json', categories)
-        json_data = self.read_from_json(f'{self.BASE_FILE_PATH}/json/kramov_categories.json')
-        self.get_items_url_from_category(json_data)
-        self.write_to_json(f'{self.BASE_FILE_PATH}/json/kramov_categories.json', json_data)
+        # json_data = self.read_from_json(f'{self.BASE_FILE_PATH}/json/kramov_categories.json')
+        # self.get_items_url_from_category(json_data)
+        # self.write_to_json(f'{self.BASE_FILE_PATH}/json/kramov_categories.json', json_data)
+        json_data = self.read_from_json(f'{self.BASE_FILE_PATH}/json/kramov_categories.json', issearch=False)
+        self.get_data(json_data)
 
 
 parser = KramovParser()
